a9ab7b08526a7d8196358fa2f78aed472ad6e8d8
ms-benet <rmenabenet@gmail.com>
2024-06-16 21:45:54 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+function loadContent(page) {
+   if (page == "About") {
+    document.getElementById("main-content").innerHTML =
+    "<object type='text/html'data='About.html'></object>";
+   } else if (page =="Projects") {
+    document.getElementById("main-content").innerHTML = 
+    "<object type='text/html'data='Projects.html'></object>";
+   } else if (page =="Contact") {
+    document.getElementById("main-content").innerHTML = 
+    "<object type='text/html'data='Contact.html'></object>";
+   }
